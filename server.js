de8d7952bb8cfada6a2dce3015885ee1f1297869
manojnaik18 <122071370+manojnaik18@users.noreply.github.com>
2025-10-15 21:31:43 +0530

    update

--- server.js
+++ server.js
@@ -10,7 +10,18 @@ const authRoutes = require('./routes/auth');
 const authMiddleware = require('./routes/authMiddleware');
 
 const app = express();
-app.use(cors());
+
+// CORS Configuration
+const allowedOrigins = ['https://teachdashboard.netlify.app', 'http://localhost:5173', 'http://localhost:3000'];
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 app.use(express.json());
 
 const seedUsers = async () => {
